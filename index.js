9776fdcb1fbe4531bd495585dbde2e442f7055c4
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-07 12:05:18 -0400

    object manipulation practice

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "pedro",
+    address: "123 Sesame Street"
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {
+        ...employee,
+        [key]: value
+    }
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value
+    return employee
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const newEmployee = { ...employee }
+    delete newEmployee[key]
+    return newEmployee
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]
+    return employee
+}
+
